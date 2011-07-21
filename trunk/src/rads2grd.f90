d639c4942e617a6c472a123e90ec6dbe5910ab46
remko@altimetrics.com <remko@altimetrics.com@0eadd5c3-4c4a-dda6-6260-284ebd57a459>
2011-07-21 17:01:51 +0000

    - Multi-satellite processing added

--- trunk/src/rads2grd.f90
+++ trunk/src/rads2grd.f90
@@ -27,7 +27,7 @@ program rads2grd
 use rads
 
 character(len=80) :: arg, grid_name='', format_string=''
-integer(fourbyteint) :: minnr=2, n(2), k(2), i, j, cycle, pass, ios
+integer(fourbyteint) :: minnr=2, n(2), k(2), i, j, l, cycle, pass, ios
 real(eightbytereal) :: lo(2), hi(2), res(2)=1d0, x, y
 logical :: c(2)=.false.
 type :: stat
@@ -36,29 +36,33 @@ type :: stat
 endtype
 type(stat), allocatable :: box(:,:)
 real(eightbytereal), allocatable :: data(:,:)
-type(rads_sat) :: S
+integer(fourbyteint), parameter :: msat = 5
+type(rads_sat) :: S(msat)
 type(rads_pass) :: P
 
 ! Initialize RADS or issue help
 call synopsis
 call rads_init (S)
-if (S%error /= rads_noerr) call rads_exit ('Fatal error')
+if (any(S%error /= rads_noerr)) call rads_exit ('Fatal error')
 
 ! If no sel= was given, use default (lon,lat,sla)
 ! If not three items, give error
-if (S%nsel == 0) then
-	call rads_parse_varlist (S, 'lon,lat,sla')
-else if (S%nsel /= 3) then
-	call rads_exit ('sel= needs exactly three elements')
-endif
+do j = 1,msat
+	if (S(j)%sat == '') exit
+	if (S(j)%nsel == 0) then
+		call rads_parse_varlist (S(j), 'lon,lat,sla')
+	else if (S(j)%nsel /= 3) then
+		call rads_exit ('sel= needs exactly three elements')
+	endif
+enddo
 
 ! Scan command line arguments
 do i = 1,iargc()
 	call getarg(i,arg)
 	if (arg(:2) == 'x=') then
-		read (arg(3:),*,iostat=ios) S%sel(1)%info%limits,res(1)
+		read (arg(3:),*,iostat=ios) S(1)%sel(1)%info%limits,res(1)
 	else if (arg(:2) == 'y=') then
-		read (arg(3:),*,iostat=ios) S%sel(2)%info%limits,res(2)
+		read (arg(3:),*,iostat=ios) S(1)%sel(2)%info%limits,res(2)
 	else if (arg(:4) == 'res=') then
 		read (arg(5:),*) res
 	else if (arg(:3) == '-Cx') then
@@ -78,8 +82,8 @@ enddo
 
 ! Set up the grid cells
 forall (j=1:2)
-	lo(j) = S%sel(j)%info%limits(1)
-	hi(j) = S%sel(j)%info%limits(2)
+	lo(j) = S(1)%sel(j)%info%limits(1)
+	hi(j) = S(1)%sel(j)%info%limits(2)
 end forall
 
 where (c)
@@ -99,36 +103,39 @@ box = stat(0, 0d0, 0d0)
 if (grid_name == '') call write_header
 
 ! Fill the grid pass by pass and keep stats, using West (1979)
-do cycle = S%cycles(1), S%cycles(2), S%cycles(3)
-	do pass = S%passes(1), S%passes(2), S%passes(3)
-		call rads_open_pass (S, P, cycle, pass)
-		if (P%ndata > 0) then
-			allocate (data(P%ndata,3))
-			do j = 1,3
-				call rads_get_var (S, P, S%sel(j), data(:,j))
-			enddo
-			do i = 1,P%ndata
-				if (any(isnan(data(i,:)))) cycle
-				k = nint((data(i,1:2) - lo)/res)+1
-				k = max(1,min(k,n))
-				call update_stat (box(k(1),k(2)), data(i,3))
-			enddo
-			deallocate (data)
-		endif
-		call rads_close_pass (S, P)
+do j = 1,msat
+	if (S(j)%sat == '') exit
+	do cycle = S(j)%cycles(1), S(j)%cycles(2), S(j)%cycles(3)
+		do pass = S(j)%passes(1), S(j)%passes(2), S(j)%passes(3)
+			call rads_open_pass (S(j), P, cycle, pass)
+			if (P%ndata > 0) then
+				allocate (data(P%ndata,3))
+				do l = 1,3
+					call rads_get_var (S(j), P, S(j)%sel(l), data(:,l))
+				enddo
+				do i = 1,P%ndata
+					if (any(isnan(data(i,:)))) cycle
+					k = nint((data(i,1:2) - lo)/res)+1
+					k = max(1,min(k,n))
+					call update_stat (box(k(1),k(2)), data(i,3))
+				enddo
+				deallocate (data)
+			endif
+			call rads_close_pass (S(j), P)
+		enddo
 	enddo
 enddo
 
 ! Print statistics if requested
-if (S%debug >= 1) call rads_stat (S)
+if (any(S%debug >= 1)) call rads_stat (S)
 
 ! Post-process grid values
 
 where (box%nr < minnr)
-	box%mean = S%nan
-	box%sum2 = S%nan
+	box%mean = S(1)%nan
+	box%sum2 = S(1)%nan
 else where (box%nr == 1)
-	box%sum2 = S%nan
+	box%sum2 = S(1)%nan
 else where
 	box%sum2 = sqrt(box%sum2/(box%nr - 1))
 end where
@@ -186,16 +193,19 @@ end subroutine update_stat
 subroutine write_header
 integer :: j
 
-600 format ('# Grid of RADS variables'/ &
-'#'/'# Satellite : ',a,'/',a/'# Cycles    :',i5,' -',i5/ &
-'# Passes    :',i5,' -',i5/'#'/'# Output columns per grid cell:'/ &
+600 format ('# Grid of RADS variables'/'#')
+610 format ('# Satellite : ',a,'/',a/'# Cycles    :',i5,' -',i5/'# Passes    :',i5,' -',i5/'#')
+620 format ('# Output columns per grid cell:'/ &
 '#   (1) ',a,' [',a,']'/ &
 '#   (2) ',a,' [',a,']'/ &
 '# (3-4) mean and stddev of ',a,' [',a,']'/ &
 '#   (5) nr of measurements')
 
-write (*,600) trim(S%sat),trim(S%phase%name),S%cycles(1:2),S%passes(1:2), &
-	(trim(S%sel(j)%info%long_name),trim(S%sel(j)%info%units),j=1,3)
+write (*,600)
+do j = 1,msat
+	if (S(j)%sat /= '') write (*,610) trim(S(j)%sat),trim(S(j)%phase%name),S(j)%cycles(1:2),S(j)%passes(1:2)
+enddo
+write (*,620) (trim(S(1)%sel(j)%info%long_name),trim(S(1)%sel(j)%info%units),j=1,3)
 end subroutine write_header
 
 !***********************************************************************
@@ -203,8 +213,8 @@ end subroutine write_header
 
 subroutine write_xyz_grid
 integer :: kx, ky
-if (format_string == '') format_string = '(' // trim(S%sel(1)%info%format) // ',1x,' // &
-	trim(S%sel(2)%info%format) // ',2(1x,' // trim(S%sel(3)%info%format) // '),i10)'
+if (format_string == '') format_string = '(' // trim(S(1)%sel(1)%info%format) // ',1x,' // &
+	trim(S(1)%sel(2)%info%format) // ',2(1x,' // trim(S(1)%sel(3)%info%format) // '),i10)'
 do ky = 1,n(2)
 	y = lo(2) + (ky-1)*res(2)
 	do kx = 1,n(1)
@@ -224,23 +234,23 @@ integer(fourbyteint) :: ncid,varid(5),dimid(2),k
 
 call nfs(nf90_create(grid_name,nf90_write+nf90_nofill,ncid))
 call nfs(nf90_put_att(ncid,nf90_global,'title',grid_name))
-call nfs(nf90_put_att(ncid,nf90_global,'history',S%command))
+call nfs(nf90_put_att(ncid,nf90_global,'history',S(1)%command))
 if (all(c)) call nfs(nf90_put_att(ncid,nf90_global,'node_offset',1))
 
 do k = 1,2
-	call nf90_def_axis(ncid,S%sel(k)%name,S%sel(k)%info%long_name,S%sel(k)%info%units,n(k), &
-		S%sel(k)%info%limits(1),S%sel(k)%info%limits(2),dimid(k),varid(k))
+	call nf90_def_axis(ncid,S(1)%sel(k)%name,S(1)%sel(k)%info%long_name,S(1)%sel(k)%info%units,n(k), &
+		S(1)%sel(k)%info%limits(1),S(1)%sel(k)%info%limits(2),dimid(k),varid(k))
 enddo
 
 call nfs(nf90_def_var(ncid,'mean'  ,nf90_real,dimid,varid(3)))
 call nfs(nf90_def_var(ncid,'stddev',nf90_real,dimid,varid(4)))
 call nfs(nf90_def_var(ncid,'nr'    ,nf90_int ,dimid,varid(5)))
-call nfs(nf90_put_att(ncid,varid(3),'long_name','mean of '//trim(S%sel(3)%info%long_name)))
-call nfs(nf90_put_att(ncid,varid(4),'long_name','std dev of '//trim(S%sel(3)%info%long_name)))
+call nfs(nf90_put_att(ncid,varid(3),'long_name','mean of '//trim(S(1)%sel(3)%info%long_name)))
+call nfs(nf90_put_att(ncid,varid(4),'long_name','std dev of '//trim(S(1)%sel(3)%info%long_name)))
 call nfs(nf90_put_att(ncid,varid(5),'long_name','number of points per cell'))
 do k = 3,4
-   call nfs(nf90_put_att(ncid,varid(k),'units',trim(S%sel(3)%info%units)))
-   call nfs(nf90_put_att(ncid,varid(k),'_FillValue',real(S%nan)))
+   call nfs(nf90_put_att(ncid,varid(k),'units',trim(S(1)%sel(3)%info%units)))
+   call nfs(nf90_put_att(ncid,varid(k),'_FillValue',real(S(1)%nan)))
 enddo
 call nfs(nf90_enddef(ncid))
 
