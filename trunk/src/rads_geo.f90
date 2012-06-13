f0a19e99c557c78923365fbae74921c84e9b7076
remko@altimetrics.com <remko@altimetrics.com@0eadd5c3-4c4a-dda6-6260-284ebd57a459>
2012-06-13 01:08:01 +0000

    rads_geo.f90 added. To contain geographical tools.

---
+++ trunk/src/rads_geo.f90
@@ -0,0 +1,73 @@
+!-----------------------------------------------------------------------
+! $Id:$
+!
+! Copyright (C) 2011  Remko Scharroo (Altimetrics LLC)
+! See LICENSE.TXT file for copying and redistribution conditions.
+!
+! This program is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!-----------------------------------------------------------------------
+
+module rads_geo
+use typesizes
+
+contains
+
+!***********************************************************************
+!*checklon - Convert and verify longitude
+!+
+function checklon (lon_bounds, lon)
+real(eightbytereal), intent(in) :: lon_bounds(2)
+real(eightbytereal), intent(inout) :: lon
+logical :: checklon
+!
+! This function converts the logitude lon (in degrees) to within limits
+! lon_bounds, where lon_bounds(1) < lon_bounds(2). If lon is not within those
+! boundaries, checklon is set to .true.
+! The longitude will be wrapped (advanced or reduced by a multiple of 360
+! degrees) if needed.
+!
+! If either of the limits is NaN, no wrapping and no check is performed.
+!
+! Arguments:
+!  lon_bounds : longitude limits (degrees)
+!  lon        : longitude (degrees)
+!  checklon   : .true. if lon is outside limits, .false. otherwise.
+!-
+if (any(isnan(lon_bounds))) then
+	checklon = .false.
+	return
+endif
+lon = lon_bounds(1) + modulo (lon-lon_bounds(1),360d0)
+checklon = (lon > lon_bounds(2))
+end function checklon
+
+!***********************************************************************
+!*sfdist - Spherical distance between two points
+!+
+function sfdist (lat0, lon0, lat1, lon1)
+real(eightbytereal), intent(in) :: lat0, lon0, lat1, lon1
+real(eightbytereal) :: sfdist
+!
+! Compute spherical distance between two points of given geocentric latitude
+! and longitude. This function is optimized for computing very small spherical
+! distances.
+!
+! Arguments:
+!  lat0, lon0 : Geocentic latitude and longitude of one point (rad).
+!  lat1, lon1 : Geocentic latitude and longitude of other point (rad).
+!  sfdist     : Spherical distance in radians.
+!-----------------------------------------------------------------------
+real(eightbytereal) :: s
+s = sin((lat0-lat1)/2d0)**2 + cos(lat0)*cos(lat1)*sin((lon0-lon1)/2d0)**2
+sfdist = asin(2d0*sqrt(s*(1-s)))
+end function sfdist
+
+end module rads_geo
