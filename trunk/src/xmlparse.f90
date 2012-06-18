8529a92c77f5f0a6048b15d1311bd78eaf9764a0
remko@altimetrics.com <remko@altimetrics.com@0eadd5c3-4c4a-dda6-6260-284ebd57a459>
2012-06-18 14:50:52 +0000

    - Added BSD license information after contacting Arjen

--- trunk/src/xmlparse.f90
+++ trunk/src/xmlparse.f90
@@ -2,9 +2,6 @@
 ! $Id$
 ! - Simple, limited XML parser in Fortran
 !
-! Adapted from the package xml-fortan-1.00 by Arjen Markus (April 2008)
-! http://xml-fortran.sourceforge.net/
-!
 ! General information:
 ! The module reads XML files by:
 ! - Identifying the tag and all attributes and data belonging
@@ -39,7 +36,10 @@
 ! Further ideas:
 ! - Simple checking via a table: parent, tag, id, min, max
 !
-! Adjusted by Remko Scharroo, Altimetrics LLC:
+! Adapted from the package xml-fortan-1.00 by Arjen Markus (April 2008)
+! http://xml-fortran.sourceforge.net/
+!
+! Adjusted by Remko Scharroo, Altimetrics LLC, July 2011:
 ! - On export, no spaces are added around data between <tag> and </tag>
 ! - When ignore_whitespace is set, whitespace is also removed upon writing
 ! - Whitespace now includes tabs
@@ -48,10 +48,32 @@
 ! - Also count first <?...?> line in the XML file
 ! - Properly report comment tags as '!--'
 ! - Keep track of level also when reading
+!
+! Copyright (c) Arjen Markus, 2008.
+!
+! Redistribution and use in source and binary forms, with or without modification,
+! are permitted provided that the following conditions are met:
+!
+! 1. Redistributions of source code must retain the above copyright notice,
+!    this list of conditions and the following disclaimer.
+! 2. Redistributions in binary form must reproduce the above copyright notice,
+!    this list of conditions and the following disclaimer in the documentation
+!    and/or other materials provided with the distribution.
+! 3. The name of the author may not be used to endorse or promote products
+!    derived from this software without specific prior written permission.
+!
+! THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
+! WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+! MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+! IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+! SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+! PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+! OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+! WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+! OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+! ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 !-----------------------------------------------------------------------
 module xmlparse
-
-implicit none
 
 integer, parameter :: xml_buffer_length = 1000
 !
