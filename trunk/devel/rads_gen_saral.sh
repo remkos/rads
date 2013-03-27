1df6824c940a719730661b839b75c154a50f1b68
remko@altimetrics.com <remko@altimetrics.com@0eadd5c3-4c4a-dda6-6260-284ebd57a459>
2013-03-27 19:14:51 +0000

    - Added rads_gen_saral.sh

--- trunk/devel/rads_gen_saral.sh
+++ trunk/devel/rads_gen_saral.sh
@@ -16,33 +16,29 @@
 # GNU Lesser General Public License for more details.
 #-----------------------------------------------------------------------
 #
-# Convert SARAL OGDR and IGDR files to RADS
+# Convert SARAL O/I/GDR files to RADS
 #
-# The most recently updated data in the OGDR and IGDR directories
-# will be processed, with the IGDRs superceding the OGDRs
+# syntax: rads_gen_saral.sh <directories>
 #-----------------------------------------------------------------------
 . radssandbox.sh
 
 rads_open_sandbox sa a
 lst=$SANDBOX/rads_gen_saral_new.lst
-imrk=igdr/.bookmark
-omrk=ogdr/.bookmark
 
 date								>  $log 2>&1
 
-# Process only OGDR data for the last three days (including current)
-
-d0=`date -u -v -2d +%Y%m%d`
-TZ=UTC touch -t ${d0}0000 $omrk
-find ogdr/c??? -name "SRL_*.nc" -a -newer $omrk | sort > $lst
-rads_gen_saral --ymd=$d0 < $lst		>> $log 2>&1
-
-# Now process all IGDR data that came in during the last four days (including current)
-
-d0=`date -u -v -3d +%Y%m%d`
-TZ=UTC touch -t ${d0}0000 $imrk
-find igdr/c??? -name "SRL_*.nc" -a -newer $imrk | sort > $lst
-rads_gen_saral < $lst				>> $log 2>&1
+for tar in $*; do
+	case $tar in
+		*.txz) tar -xJf $tar; dir=`basename $tar .txz` ;;
+		*.tgz) tar -xzf $tar; dir=`basename $tar .tgz` ;;
+		*) dir=$tar ;;
+	esac
+	ls $dir/SRL_*.nc > $lst
+	rads_gen_saral < $lst			>> $log 2>&1
+	case $tar in
+		*.t?z) chmod -R u+w $dir; rm -rf $dir ;;
+	esac
+done
 
 # Do the patches to all data
 
@@ -50,6 +46,7 @@ radsp_iono   $options jpl iri nic	>> $log 2>&1
 radsp_common $options				>> $log 2>&1
 radsp_mog2d  $options				>> $log 2>&1
 radsp_ib     $options				>> $log 2>&1
+
 date								>> $log 2>&1
 
 rads_close_sandbox
