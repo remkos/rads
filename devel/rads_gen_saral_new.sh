9d7b49b9e102a346bac3b66d46aa7ff20e194cf2
Remko Scharroo <remko@altimetrics.com>
2013-07-14 20:33:42 +0000

    Added WW3 model interpolation

--- devel/rads_gen_saral_new.sh
+++ devel/rads_gen_saral_new.sh
@@ -46,12 +46,13 @@ rads_gen_saral < $lst				>> $log 2>&1
 
 # Do the patches to all data
 
-rads_add_iono   $options --all		>> $log 2>&1
-rads_add_common $options			>> $log 2>&1
-rads_add_ncep   $options -gdwsu		>> $log 2>&1
-rads_fix_sa     $options --all		>> $log 2>&1
-rads_add_mog2d  $options			>> $log 2>&1
-rads_add_ib     $options			>> $log 2>&1
+rads_add_iono    $options --all		>> $log 2>&1
+rads_add_common  $options			>> $log 2>&1
+rads_add_ncep    $options -gdwsu	>> $log 2>&1
+rads_fix_sa      $options --all		>> $log 2>&1
+rads_add_mog2d   $options			>> $log 2>&1
+rads_add_ib      $options			>> $log 2>&1
+rads_add_ww3_222 $options --all		>> $log 2>&1
 
 date								>> $log 2>&1
 
