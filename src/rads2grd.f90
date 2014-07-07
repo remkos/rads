451243291db752500209347684d731ac23aa9d53
Remko Scharroo <remko@altimetrics.com>
2014-07-07 11:31:42 +0000

    Numerous updates to rads2grd: - Properly calculate number of bins - Set limits based on --x and --y, when given - Allow multiple z variables

--- src/rads2grd.f90
+++ src/rads2grd.f90
@@ -30,14 +30,14 @@ use rads_time
 
 character(len=rads_cmdl) :: grid_name=''
 character(len=rads_strl) :: format_string=''
-integer(fourbyteint) :: minnr=2, n(2), k(2), i, j, l, cycle, pass, ios
+integer(fourbyteint) :: minnr=2, n(2), k(2), i, j, l, cycle, pass, ios, nvar, nsat
 real(eightbytereal) :: lo(2), hi(2), res(2), x, y, limits(3,2)
 logical :: c(2)=.false.
 type :: stat
 	integer(fourbyteint) :: nr
 	real(eightbytereal) :: mean, sum2
 endtype
-type(stat), allocatable :: box(:,:)
+type(stat), allocatable :: box(:,:,:)
 real(eightbytereal), allocatable :: data(:,:)
 integer(fourbyteint), parameter :: msat = 20
 type(rads_sat) :: S(msat)
@@ -51,14 +51,18 @@ if (any(S%error /= rads_noerr)) call rads_exit ('Fatal error')
 
 ! If no sel= was given, use default (lon,lat,sla)
 ! If not three items, give error
+nsat = 0
 do j = 1,msat
 	if (S(j)%sat == '') exit
 	if (S(j)%nsel == 0) then
 		call rads_parse_varlist (S(j), 'lon,lat,sla')
-	else if (S(j)%nsel /= 3) then
-		call rads_exit ('-V|--var= needs exactly three elements')
+	else if (S(j)%nsel < 3) then
+		call rads_exit ('-V|--var= needs at least three elements')
 	endif
+	nsat = nsat + 1
 enddo
+nvar = S(1)%nsel - 2
+if (nsat == 0) call rads_exit ('Specify at least one satellite')
 
 ! Set default cell sizes
 limits(1:2,1) = S(1)%sel(1)%info%limits
@@ -98,13 +102,18 @@ if (any(isnan_(limits(:,2)))) call rads_exit ('You have not set the range or res
 lo  = limits(1,:)
 hi  = limits(2,:)
 res = limits(3,:)
+do j = 1,nsat
+	do i = 1,2
+		S(j)%sel(i)%info%limits = (/lo(i),hi(i)/)
+	enddo
+enddo
 where (c)
 	lo = lo + 0.5d0 * res
 	hi = hi - 0.5d0 * res
 endwhere
-n = nint((hi-lo)/res+0.999d0)
+n = nint((hi-lo)/res+1d0)
 
-allocate (box(n(1),n(2)),stat=ios)
+allocate (box(n(1),n(2),nvar+2),stat=ios)
 if (ios /= 0) then
 	write (format_string, '(i0,"x",i0," points")') n, n
 	call rads_exit ('Unable to allocate memory for '//trim(format_string))
@@ -121,15 +130,17 @@ do j = 1,msat
 		do pass = S(j)%passes(1), S(j)%passes(2), S(j)%passes(3)
 			call rads_open_pass (S(j), P, cycle, pass)
 			if (P%ndata > 0) then
-				allocate (data(P%ndata,3))
-				do l = 1,3
+				allocate (data(P%ndata,nvar+2))
+				do l = 1,nvar+2
 					call rads_get_var (S(j), P, S(j)%sel(l), data(:,l))
 				enddo
 				do i = 1,P%ndata
 					if (any(isnan_(data(i,:)))) cycle
 					k = nint((data(i,1:2) - lo)/res)+1
 					k = max(1,min(k,n))
-					call update_stat (box(k(1),k(2)), data(i,3))
+					do l = 1,nvar
+						call update_stat (box(k(1),k(2),l), data(i,l+2))
+					enddo
 				enddo
 				deallocate (data)
 			endif
@@ -153,7 +164,7 @@ endwhere
 if (grid_name == '') then
 	call write_xyz_grid
 else
-	call write_nc_grid
+	call write_nc_grid (nvar)
 endif
 
 ! Print statistics if requested
@@ -173,7 +184,7 @@ call rads_synopsis
 write (*,1300)
 1300 format (/ &
 'Program specific [program_options] are:'/ &
-'  -V, --var=X,Y,Z           Variables for x, y and z (default = lon,lat,sla)'/ &
+'  -V, --var=X,Y,Z           Variables for x, y and z (default = lon,lat,sla); multiple z''s may be specified' / &
 '  --x=X0,X1[,DX]            Set x-range and interval (default: as set by default limits and --res=)'/ &
 '  --y=Y0,Y1[,DY]            Set y-range and interval (default: as set by default limits and --res=)'/ &
 '  --res=DX[,DY]             Set resolution in x and y (default = 1)'/ &
@@ -209,16 +220,20 @@ integer :: j
 600 format ('# Grid of RADS variables'/'# Created: ',a,' UTC: ',a)
 610 format ('#'/'# Satellite : ',a,'/',a/'# Cycles    :',i5,' -',i5/'# Passes    :',i5,' -',i5/'#')
 620 format ('# Output columns per grid cell:'/ &
-'#   (1) ',a,' [',a,']'/ &
-'#   (2) ',a,' [',a,']'/ &
-'# (3-4) mean and stddev of ',a,' [',a,']'/ &
-'#   (5) nr of measurements')
+'#    ( 1) ',a,' [',a,']'/ &
+'#    ( 2) ',a,' [',a,']')
+621 format ('# (',i2,'-',i2,') mean and stddev of ',a,' [',a,']')
+622 format ('#    (',i2,') nr of measurements')
 
 write (*,600) timestamp(), trim(S(1)%command)
 do j = 1,msat
 	if (S(j)%sat /= '') write (*,610) trim(S(j)%sat),trim(S(j)%phase%name),S(j)%cycles(1:2),S(j)%passes(1:2)
 enddo
-write (*,620) (trim(S(1)%sel(j)%long_name),trim(S(1)%sel(j)%info%units),j=1,3)
+write (*,620) (trim(S(1)%sel(j)%long_name),trim(S(1)%sel(j)%info%units),j=1,2)
+do j = 1,nvar
+	write (*,621) j*2+1,j*2+2,trim(S(1)%sel(j+2)%long_name),trim(S(1)%sel(j+2)%info%units)
+enddo
+write (*,622) nvar*2+3
 end subroutine write_header
 
 !***********************************************************************
@@ -226,13 +241,18 @@ end subroutine write_header
 
 subroutine write_xyz_grid
 integer :: kx, ky
-if (format_string == '') format_string = '(' // trim(S(1)%sel(1)%info%format) // ',1x,' // &
-	trim(S(1)%sel(2)%info%format) // ',2(1x,' // trim(S(1)%sel(3)%info%format) // '),1x,i0)'
+if (format_string == '') then
+	format_string = '(' // trim(S(1)%sel(1)%info%format) // ',1x,' // trim(S(1)%sel(2)%info%format)
+	do j = 1,nvar
+		format_string = trim(format_string) // ',2(1x,' // trim(S(1)%sel(3)%info%format) // ')'
+	enddo
+	format_string = trim(format_string) // ',1x,i0)'
+endif
 do ky = 1,n(2)
 	y = lo(2) + (ky-1)*res(2)
 	do kx = 1,n(1)
 		x = lo(1) + (kx-1)*res(1)
-		if (box(kx,ky)%nr >= minnr) write (*,format_string) x,y,box(kx,ky)%mean,box(kx,ky)%sum2,box(kx,ky)%nr
+		if (box(kx,ky,1)%nr >= minnr) write (*,format_string) x,y,(box(kx,ky,j)%mean,box(kx,ky,j)%sum2,j=1,nvar),box(kx,ky,1)%nr
 	enddo
 enddo
 end subroutine write_xyz_grid
@@ -240,12 +260,14 @@ end subroutine write_xyz_grid
 !***********************************************************************
 ! Write out netCDF grid
 
-subroutine write_nc_grid
+subroutine write_nc_grid (nvar)
 use netcdf
 use rads_netcdf
 use rads_time
-integer(fourbyteint) :: ncid,varid(5),dimid(2),k
-real(fourbytereal), parameter :: nan = transfer (not(0_fourbyteint),0_fourbytereal)
+integer(fourbyteint), intent(in) :: nvar
+integer(fourbyteint) :: ncid,varid(nvar*2+3),dimid(2),j,k
+real(fourbytereal), parameter :: nan = transfer (not(0_fourbyteint),0e0)
+character(len=rads_varl) :: ext
 
 call nfs (nf90_create(grid_name,nf90_write+nf90_nofill,ncid))
 call nfs (nf90_put_att (ncid, nf90_global, 'Conventions', 'CF-1.5'))
@@ -255,27 +277,33 @@ if (all(c)) call nfs (nf90_put_att(ncid,nf90_global,'node_offset',1))
 
 do k = 1,2
 	call nf90_def_axis(ncid,S(1)%sel(k)%name,S(1)%sel(k)%long_name,S(1)%sel(k)%info%units,n(k), &
-		S(1)%sel(k)%info%limits(1),S(1)%sel(k)%info%limits(2),dimid(k),varid(k))
+		lo(k),hi(k),dimid(k),varid(k))
 enddo
 
-call nfs (nf90_def_var(ncid,'mean'  ,nf90_real,dimid,varid(3)))
-call nfs (nf90_def_var(ncid,'stddev',nf90_real,dimid,varid(4)))
-call nfs (nf90_def_var(ncid,'nr'    ,nf90_int ,dimid,varid(5)))
-call nfs (nf90_put_att(ncid,varid(3),'long_name','mean of '//trim(S(1)%sel(3)%long_name)))
-call nfs (nf90_put_att(ncid,varid(4),'long_name','std dev of '//trim(S(1)%sel(3)%long_name)))
-call nfs (nf90_put_att(ncid,varid(5),'long_name','number of points per cell'))
-do k = 3,4
-	call nfs (nf90_put_att(ncid,varid(k),'units',trim(S(1)%sel(3)%info%units)))
-	call nfs (nf90_put_att(ncid,varid(k),'_FillValue',nan))
+ext = ''
+do j = 1,nvar
+	if (nvar /= 1) ext = trim(S(1)%sel(j+2)%name) // '_'
+	call nfs (nf90_def_var(ncid,trim(ext) // 'mean'  ,nf90_real,dimid,varid(j*2+1)))
+	call nfs (nf90_def_var(ncid,trim(ext) // 'stddev',nf90_real,dimid,varid(j*2+2)))
+	call nfs (nf90_put_att(ncid,varid(j*2+1),'long_name','mean of '//trim(S(1)%sel(j+2)%long_name)))
+	call nfs (nf90_put_att(ncid,varid(j*2+2),'long_name','std dev of '//trim(S(1)%sel(j+2)%long_name)))
+	do k = 1,2
+		call nfs (nf90_put_att(ncid,varid(j*2+k),'units',trim(S(1)%sel(j+2)%info%units)))
+		call nfs (nf90_put_att(ncid,varid(j*2+k),'_FillValue',nan))
+	enddo
 enddo
+call nfs (nf90_def_var(ncid,'nr',nf90_int,dimid,varid(nvar*2+3)))
+call nfs (nf90_put_att(ncid,varid(nvar*2+3),'long_name','number of points per cell'))
 call nfs (nf90_enddef(ncid))
 
 call nf90_put_axis(ncid,varid(1))
 call nf90_put_axis(ncid,varid(2))
 
-call nfs (nf90_put_var(ncid,varid(3),box%mean))
-call nfs (nf90_put_var(ncid,varid(4),box%sum2))
-call nfs (nf90_put_var(ncid,varid(5),box%nr))
+do j = 1,nvar
+	call nfs (nf90_put_var(ncid,varid(j*2+1),box(:,:,j)%mean))
+	call nfs (nf90_put_var(ncid,varid(j*2+2),box(:,:,j)%sum2))
+enddo
+call nfs (nf90_put_var(ncid,varid(nvar*2+3),box%nr))
 call nfs (nf90_close(ncid))
 end subroutine write_nc_grid
 
