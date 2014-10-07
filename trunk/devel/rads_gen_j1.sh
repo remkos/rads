4540a9076c8f8a122583527ecd96a7a4afe016cd
leuliett@gmail.com <leuliett@gmail.com@0eadd5c3-4c4a-dda6-6260-284ebd57a459>
2014-10-07 15:44:17 +0000

    Add Tran 2012 to rads_gen_j1.sh

--- trunk/devel/rads_gen_j1.sh
+++ trunk/devel/rads_gen_j1.sh
@@ -47,6 +47,7 @@ rads_add_iono    $options --all							>> $log 2>&1
 rads_add_common  $options								>> $log 2>&1
 rads_add_dual    $options								>> $log 2>&1
 rads_add_ib      $options								>> $log 2>&1
+rads_add_ssb     $options --ssb=ssb_tran2012				>> $log 2>&1
 rads_add_orbit   $options -Valt_gdrd					>> $log 2>&1
 rads_add_orbit   $options -Valt_eig6					>> $log 2>&1
 rads_add_orbit   $options -Valt_gdrcp   -C1-255			>> $log 2>&1
