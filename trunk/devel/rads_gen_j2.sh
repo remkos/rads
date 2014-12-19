1623ed86435a88188b7e89df439e530a4e1e73b7
remko@altimetrics.com <remko@altimetrics.com@0eadd5c3-4c4a-dda6-6260-284ebd57a459>
2014-12-19 13:24:37 +0000

    Add alt_std1404 to rads_gen_j2.sh (cycles 0-188)

--- trunk/devel/rads_gen_j2.sh
+++ trunk/devel/rads_gen_j2.sh
@@ -53,7 +53,7 @@ rads_add_ib      $options						>> $log 2>&1
 rads_add_orbit   $options -Valt_eig6			>> $log 2>&1
 rads_add_orbit   $options -Valt_gdrcp   -C1-130	>> $log 2>&1
 rads_add_orbit   $options -Valt_gps     -C1-225	>> $log 2>&1
-rads_add_orbit   $options -Valt_std1204 -C1-195	>> $log 2>&1
+rads_add_orbit   $options -Valt_std1204 -C0-188	>> $log 2>&1
 rads_add_orbit   $options -Valt_std1404			>> $log 2>&1
 rads_add_ww3_222 $options --all	-C165-300		>> $log 2>&1
 rads_add_ww3_314 $options --all -C0-165			>> $log 2>&1
