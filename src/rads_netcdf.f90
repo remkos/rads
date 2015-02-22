e008f4ce8adc0b2ddf4930a5dd44c8d1a88daa30
Remko Scharroo <remko@altimetrics.com>
2015-02-22 16:50:41 +0000

    Made use of nf90_inq_path in nf90_message depended on --with-devel This is because nf90_inq_path was only introduced in July 2014.

--- src/rads_netcdf.f90
+++ src/rads_netcdf.f90
@@ -59,39 +59,7 @@ end interface
 
 contains
 
-!-----------------------------------------------------------------------
-!*nf90_message -- Print message to standard error
-!+
-subroutine nf90_message (string, ncid)
-use netcdf
-character(len=*), intent(in) :: string
-integer(fourbyteint), optional :: ncid
-!
-! This routine prints a message to standard error, while prepending
-! the program name.
-! Optionally, it adds the file name (based on the netCDF ID) to which
-! the error pertains.
-!
-! Examples:
-! call nf90_message ('hello world')
-!     output => program: hello word
-! call nf90_message ('error occurred in', ncid)
-!     output => program: error occurred in filename
-!
-! Arguments:
-!  string   : Error message string
-!  ncid     : Optional: netCDF ID providing the filename
-!-----------------------------------------------------------------------
-character(len=160) :: progname, filename
-integer :: i, l
-call getarg (0, progname)
-if (present(ncid)) then
-	i = nf90_inq_path (ncid, l, filename)
-	write (stderr, '(a,": ",a,1x,a)') trim(progname), trim(string), filename(:l)
-else
-	write (stderr, '(a,": ",a)') trim(progname), trim(string)
-endif
-end subroutine nf90_message
+include "nf90_message.f90"
 
 !-----------------------------------------------------------------------
 !*nf90_inq_varid_warn -- Get the netCDF ID for a variable, with warning
@@ -275,10 +243,8 @@ integer, intent(in) :: ios
 ! Example:
 ! call nfs (nf90_open ('file.nc', nf90_write, ncid))
 !-----------------------------------------------------------------------
-character(len=80) :: prognm
-if (ios==nf90_noerr) return
-call getarg (0, prognm)
-write (stderr, '(a,": ",a)') trim(prognm),trim(nf90_strerror(ios))
+if (ios == nf90_noerr) return
+call nf90_message (nf90_strerror(ios))
 call exit (ios)
 end subroutine nfs
 
