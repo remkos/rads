cd58c473684e232d263ea0ae351ac72cf4aa210d
Remko Scharroo <remko@altimetrics.com>
2015-08-26 14:18:19 +0200

    Update rads_sample.f90

--- src/rads_sample.f90
+++ src/rads_sample.f90
@@ -15,18 +15,18 @@
 
 program rads_sample
 use rads                    ! Use rads module
-type(rads_sat) :: S         ! Define a structure that contains all mission information
+type(rads_sat) :: S         ! Structure that contains all mission information
 type(rads_pass) :: P        ! Structure for pass information
 real(eightbytereal), allocatable :: lon(:), lat(:), sla(:)
 integer :: cycle, pass, i
-call rads_init (S, 'e1')    ! Initialize RADS. Select ERS-1. No mission needed!
-do cycle = 1,30
-    do pass = 1,10
+call rads_init (S, 'e1')    ! Initialize RADS. Select ERS-1. No mission phase needed!
+do cycle = 1, 30            ! Loop through 30 cyles
+    do pass = 1, 10, 2      ! List ascending passes 1-9
         call rads_open_pass (S, P, cycle, pass)              ! Open pass file
         allocate (lon(P%ndata), lat(P%ndata), sla(P%ndata))  ! Allocate memory
-        call rads_get_var (S, P, 'lon', lon)                 ! Get the data
-        call rads_get_var (S, P, 'lat', lat)
-        call rads_get_var (S, P, 'sla', sla)
+        call rads_get_var (S, P, 'lon', lon)                 ! Get longitude
+        call rads_get_var (S, P, 'lat', lat)                 ! Get latitude
+        call rads_get_var (S, P, 'sla', sla)                 ! Get sea level anomaly
         do i = 1,P%ndata
             write (*,*) lon(i), lat(i), sla(i)               ! Print the data
         enddo
@@ -34,5 +34,5 @@ do cycle = 1,30
         call rads_close_pass (S, P)                          ! Close pass
     enddo
 enddo
-call rads_end (S)                                            ! End
+call rads_end (S)           ! End RADS; free data structure
 end program rads_sample
