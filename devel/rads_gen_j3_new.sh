67100c58d5e49cbd63394bf63edacb4f8b318ecd
Remko Scharroo <remko@altimetrics.com>
2016-01-27 14:10:20 +0100

    Allow to select ogdr and igdr separately in rads_gen_j3_new.sh

--- devel/rads_gen_j3_new.sh
+++ devel/rads_gen_j3_new.sh
@@ -29,7 +29,10 @@
 
 d0=`date -u -v -9d +%Y%m%d 2>&1` || d0=`date -u --date="9 days ago" +%Y%m%d`
 
-for type in ogdr igdr; do
+# Run this for ogdr or igdr depending on the arguments on the command line.
+# Default is doing both!
+
+for type in ${1:-ogdr igdr}; do
 rads_open_sandbox j3.${type}0
 lst=$SANDBOX/rads_gen_j3_tmp.lst
 
