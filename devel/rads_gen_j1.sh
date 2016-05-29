69472bf5159a125d49dca756fe15e60216ee3024
Remko Scharroo <remko@altimetrics.com>
2016-05-29 15:52:04 +0200

    New SLCCI orbits for Jason-1 and Jason-2 in scripts.

--- devel/rads_gen_j1.sh
+++ devel/rads_gen_j1.sh
@@ -47,11 +47,12 @@ rads_add_dual    $options								>> $log 2>&1
 rads_add_ib      $options								>> $log 2>&1
 rads_add_ssb     $options --ssb=ssb_tran2012			>> $log 2>&1
 rads_add_orbit   $options -Valt_gdrd					>> $log 2>&1
-rads_add_orbit   $options -Valt_gdre    -C111-425		>> $log 2>&1
+rads_add_orbit   $options -Valt_gdre					>> $log 2>&1
 rads_add_orbit   $options -Valt_eig62s  -C1-260			>> $log 2>&1
 rads_add_orbit   $options -Valt_gdrcp   -C1-255			>> $log 2>&1
 rads_add_orbit   $options -Valt_std1204	-C1-259			>> $log 2>&1
 rads_add_orbit   $options -Valt_gps     -C9-161			>> $log 2>&1
+rads_add_orbit   $options -Valt_slcci					>> $log 2>&1
 rads_add_ww3_314 $options --ww3	-C1-406					>> $log 2>&1
 rads_add_sla     $options           					>> $log 2>&1
 
