741bf6beeb13f16ac38fa7ccdfcedd1d5c7027a3
Remko Scharroo <remko@altimetrics.com>
2017-06-14 23:44:37 +0200

    Back to min_lat equal to 0.1 * dt

--- devel/make_orf.f90
+++ devel/make_orf.f90
@@ -92,7 +92,7 @@ endif
 
 ! Determine threshold of latitudes
 
-min_lat = dt * 0.2d0	! Keep significant margin with 0.2 degrees per second
+min_lat = dt * 0.1d0	! Keep significant margin with 0.1 degrees per second
 max_lat = min(S%inclination, 180d0 - S%inclination) - min_lat
 
 ! Start with start time
