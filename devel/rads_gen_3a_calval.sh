7083d18a690d5cf9cfa4f5a4e2ec14b039a2ae90
Remko Scharroo <remko@altimetrics.com>
2017-07-07 14:31:46 +0200

    Activate rads_add_iono in two rads_gen_3a scripts

--- devel/rads_gen_3a_calval.sh
+++ devel/rads_gen_3a_calval.sh
@@ -62,7 +62,7 @@ rads_add_common $options							>> $log 2>&1
 rads_add_mog2d  $options							>> $log 2>&1
 rads_add_ncep   $options -gdwi						>> $log 2>&1
 rads_add_ecmwf  $options -dwui						>> $log 2>&1
-rads_add_iono   $options -all						>> $log 2>&1
+rads_add_iono   $options --all						>> $log 2>&1
 # Redetermine SSHA
 rads_add_sla    $options							>> $log 2>&1
 rads_add_sla    $options --ext=plrm					>> $log 2>&1
