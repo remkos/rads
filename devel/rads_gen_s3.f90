remkos/rads

commit 17eef8b127980e1414c937299b7355a461fbe6f4

Detect and label "reprocessing latency" for Sentinel-3

devel/rads_gen_s3.f90
@@ -103,7 +103,6 @@ character(len=rads_cmdl) :: infile, arg
 
 integer(fourbyteint) :: cyclenr, passnr, varid, orbit_type_offset
 real(eightbytereal) :: equator_time
-logical :: nrt, stc, ntc
 
 ! Data variables
 
@@ -181,10 +180,31 @@ do
 		cycle
 	endif
 
+! Determine latency
+
 	call nfs(nf90_get_att(ncid,nf90_global,'product_name',arg))
-	nrt = index(arg,'_NR_') > 0
-	stc = index(arg,'_ST_') > 0
-	ntc = index(arg,'_NT_') > 0
+	if (index(arg, '_NR_') > 0) then
+		latency = rads_nrt
+	else if (index(arg, '_ST_') > 0) then
+		latency = rads_stc
+	else if (index(arg, '_NT_') > 0) then
+		latency = rads_ntc
+	else
+		call log_string ('Error: file skipped: unknown latency', .true.)
+		cycle
+	endif
+
+! Detect REP_006 (S3A) or REP_007 (S3B)
+
+	if (index(arg, '_R_NT_003') == 0) then
+	else if (S%sat == '3a') then
+		latency = latency + 6 ! REP_006
+	else
+		latency = latency + 7 ! REP_007
+	endif
+
+! Read cycle, pass, equator time
+
 	call nfs(nf90_get_att(ncid,nf90_global,'cycle_number',cyclenr))
 	call nfs(nf90_get_att(ncid,nf90_global,'pass_number',passnr))
 	call nfs(nf90_get_att(ncid,nf90_global,'equator_time',arg))
@@ -323,7 +343,7 @@ do
 	call cpy_var ('iono_cor_alt_01_plrm_ku', 'iono_alt_plrm')
 	call cpy_var ('iono_cor_gim_01_ku', 'iono_gim')
 	call cpy_var ('inv_bar_cor_01', 'inv_bar_static')
-	if (nrt) then
+	if (latency(i) == rads_nrt) then
 		call cpy_var ('inv_bar_cor_01', 'inv_bar_mog2d')
 	else
 		call cpy_var ('inv_bar_cor_01 hf_fluct_cor_01 ADD', 'inv_bar_mog2d')
@@ -395,10 +415,7 @@ do
 		call new_var ('orbit_type', a)
 	endif
 	call cpy_var ('surf_class_01', 'surface_class')
-	if (nrt) latency(:) = 0
-	if (stc) latency(:) = 1
-	if (ntc) latency(:) = 2
-	call new_var ('latency',latency)
+	call new_var ('latency', latency)
 
 ! Dump the data
 

conf/rads.xml
@@ -1652,8 +1652,8 @@ Global RADS configuration file
 
 <var name="latency">
 	<long_name>source product latency flag</long_name>
-	<flag_values>NRT_OGDR STC_IGDR NTC_GDR</flag_values>
-	<plot_range>0 2</plot_range>
+	<flag_values>NRT_OGDR STC_IGDR NTC_GDR REP_001 REP_002 REP_003 REP_004 REP_005 REP_006 REP_007 REP_008</flag_values>
+	<plot_range>0 10</plot_range>
 	<data>latency</data>
 	<format>z1</format>
 	<compress>int1</compress>

commit ac84ff7a30864341f02482f3c75a83a7e1cacc7d

Fix bug introduced by commit 17eef8b

devel/rads_gen_s3.f90
@@ -108,7 +108,7 @@ real(eightbytereal) :: equator_time
 
 integer(twobyteint), allocatable :: flags_plrm(:), flags_save(:)
 character(len=2) :: mss_cnescls_ver, mss_dtu_ver, tide_fes_ver
-real(eightbytereal), allocatable :: latency(:)
+integer :: latency = rads_nrt
 
 ! Other local variables
 
@@ -279,7 +279,7 @@ do
 
 ! Allocate variables
 
-	allocate (a(nrec),dh(nrec),flags(nrec),flags_plrm(nrec),flags_save(nrec),latency(nrec))
+	allocate (a(nrec),dh(nrec),flags(nrec),flags_plrm(nrec),flags_save(nrec))
 	nvar = 0
 
 ! Compile flag bits
@@ -347,7 +347,7 @@ do
 	call cpy_var ('iono_cor_alt_01_plrm_ku', 'iono_alt_plrm')
 	call cpy_var ('iono_cor_gim_01_ku', 'iono_gim')
 	call cpy_var ('inv_bar_cor_01', 'inv_bar_static')
-	if (latency(i) == rads_nrt) then
+	if (latency == rads_nrt) then
 		call cpy_var ('inv_bar_cor_01', 'inv_bar_mog2d')
 	else
 		call cpy_var ('inv_bar_cor_01 hf_fluct_cor_01 ADD', 'inv_bar_mog2d')
@@ -419,13 +419,14 @@ do
 		call new_var ('orbit_type', a)
 	endif
 	call cpy_var ('surf_class_01', 'surface_class')
-	call new_var ('latency', latency)
+	a = latency
+	call new_var ('latency', a)
 
 ! Dump the data
 
 	call nfs(nf90_close(ncid))
 	call put_rads
-	deallocate (a, dh, flags, flags_plrm, flags_save, latency)
+	deallocate (a, dh, flags, flags_plrm, flags_save)
 
 enddo
 