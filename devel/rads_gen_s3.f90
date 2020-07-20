5472b2209a7383f3bd3bd75bc3d08e8fed115292
Remko Scharroo <remko@altimetrics.com>
2020-07-20 10:49:41 +0200

    Update copyright to 2020

--- devel/rads_gen_s3.f90
+++ devel/rads_gen_s3.f90
@@ -1,5 +1,5 @@
 !-----------------------------------------------------------------------
-! Copyright (c) 2011-2019  Remko Scharroo and Eric Leuliette
+! Copyright (c) 2011-2020  Remko Scharroo and Eric Leuliette
 ! See LICENSE.TXT file for copying and redistribution conditions.
 !
 ! This program is free software: you can redistribute it and/or modify
