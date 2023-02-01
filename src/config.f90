8a188de88e5d4d115ea94c943fae66e94ae14f20
Eric Leuliette <eric.leuliette@noaa.gov>
2023-02-01 17:12:22 -0500

    Clean out configuration files and compiled code

--- src/config.f90
+++
@@ -1,3 +0,0 @@
-character(len=rads_naml), parameter :: radsdataroot = '/Users/davidtrossman/Documents/Code/rads/data'
-logical, parameter :: little_endian = .true.
-character(len=rads_varl), parameter :: rads_version_id = '4.4.0'
