c6f5cf3358a6faa476eff42c90cdc17204326cdf
Remko Scharroo <remko@altimetrics.com>
2023-03-23 13:38:51 +0100

    hardlink.sh - spelling fixes

--- devel/hardlink.sh
+++ devel/hardlink.sh
@@ -4,21 +4,21 @@
 
 if test $# -le 1 ; then
 	cat <<%
-$0 - Create hardlinks of new files from one directory structure to another
+hardlink.sh - Create hardlinks of new files from one directory structure to another
 
-syntax: $0 from_dir to_dir [ find_options ]
+syntax: hardlink.sh from_dir to_dir [ find_options ]
 
 where:
 	from_dir     : directory where to find new files
 	to_dir       : directory where to hardlink new files to (to_dir replaces from_dir in the pathname)
 	find_options : options to add to the find command for example:
-	               -mtime -1 : only look for files less than 24 hour old
-	               -links 1  : only look for files that are not yet hardlinked
+	              -mtime -1 : only look for files less than 24 hour old
+	              -links 1  : only look for files that are not yet hardlinked
 
-This command first looks for files in from_dir using the find commond with find_options.
-Each file that does not exist in the same place in to_dir, or has the same or old time step in to_dir
-is then hardlinked from from_dir to to_dir.
-Missing directories are created if needed.
+This command first looks for files in from_dir using the find command with find_options.
+Each selected file from from_dir that does not exist in the same place in to_dir, or has the same or
+older time stamp in to_dir is then hardlinked from from_dir to to_dir.
+Missing directories in to_dir are created if needed.
 %
 	exit
 fi
