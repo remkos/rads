272e7488ff1929d636c1e726638e03911522f851
Remko Scharroo <remko@altimetrics.com>
2023-05-05 10:00:34 +0200

    hardlink.sh: introduce -f option

--- devel/hardlink.sh
+++ devel/hardlink.sh
@@ -6,18 +6,20 @@ if test $# -le 1 ; then
 	cat <<%
 hardlink.sh - Create hardlinks of new files from one directory structure to another
 
-syntax: hardlink.sh from_dir to_dir [ find_options ]
+syntax: hardlink.sh from_dir to_dir [ -f ] [ find_options ]
 
 where:
 	from_dir     : directory where to find new files
 	to_dir       : directory where to hardlink new files to (to_dir replaces from_dir in the pathname)
+	-f           : make link irrespective of modification time of files
 	find_options : options to add to the find command for example:
 	              -mtime -1 : only look for files less than 24 hour old
 	              -links 1  : only look for files that are not yet hardlinked
 
 This command first looks for files in from_dir using the find command with find_options.
 Each selected file from from_dir that does not exist in the same place in to_dir, or has the same or
-older time stamp in to_dir is then hardlinked from from_dir to to_dir.
+older time stamp in to_dir is then hardlinked from from_dir to to_dir. Unless -f is used: then
+the hardlink is made irrespective of the time stamps.
 Missing directories in to_dir are created if needed.
 %
 	exit
@@ -25,17 +27,16 @@ fi
 
 from_dir=$1
 to_dir=$2
+force=0
 shift 2
 
-if test -x /opt/local/bin/gstat ; then
-	stat=/opt/local/bin/gstat
-else
-	stat=stat
-fi
+case "$1" in
+	"-f") force=1 ; shift 1 ;;
+esac
 
 while read file ; do
 	new=${file/#$from_dir/$to_dir}
-	if ! test $file -ot $new ; then
+	if (! test $file -ot $new) || (test $force -eq 1) ; then
 		echo "$file -> $new"
 		mkdir -p $(dirname $new)
 		ln -f $file $new
