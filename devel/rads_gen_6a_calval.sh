61cd256949787a775b4d0240c7bec4e2b6176f1c
Remko Scharroo <remko@altimetrics.com>
2023-06-22 15:13:41 +0200

    rads_gen_6a_calval.sh: better selection of type

--- devel/rads_gen_6a_calval.sh
+++ devel/rads_gen_6a_calval.sh
@@ -67,11 +67,11 @@ if test -z $type ; then
 		*/NT*|*_NT_*) type=${type}nt ;;
 	esac
 	case $dir in
-		*REP/*|*_REP_*) type=${type:0:2}f08 ;;
-		*OPE/*|*_OPE_*) type=${type}o ;;
-		*VAL/*|*_VAL_*) type=${type}v ;;
-		*DEV/*|*_DEV_*) type=${type}d ;;
 		*RMC/*)         type=hrrmc ;;
+		*REP/*|*_REP_*) type=${type:0:2}f08 ;;
+		*DEV/*|*_DEV_*) type=${type}d ;;
+		*VAL/*|*_VAL_*) type=${type}v ;;
+		*)              type=${type}o ;;
 	esac
 fi
 
