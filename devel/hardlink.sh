74272ed01dbfff6f450457096597e9839bb6466e
Remko Scharroo <remko@altimetrics.com>
2023-06-26 13:30:12 +0200

    hardlink.sh: check on link status instead of time stamp of target file

--- devel/hardlink.sh
+++ devel/hardlink.sh
@@ -11,19 +11,21 @@ syntax: hardlink.sh from_dir to_dir [ -f ] [ find_options ]
 where:
 	from_dir     : directory where to find new files
 	to_dir       : directory where to hardlink new files to (to_dir replaces from_dir in the pathname)
-	-f           : make link irrespective of modification time of files
+	-f           : make link irrespective of whether the target file is already hardlinked
 	find_options : options to add to the find command for example:
 	              -mtime -1 : only look for files less than 24 hour old
 	              -links 1  : only look for files that are not yet hardlinked
 
 This command first looks for files in from_dir using the find command with find_options.
-Each selected file from from_dir that does not exist in the same place in to_dir, or has the same or
-older time stamp in to_dir is then hardlinked from from_dir to to_dir. Unless -f is used: then
-the hardlink is made irrespective of the time stamps.
+Each selected file from from_dir is harlinked to the same location in to_dir whenever
+the target does not yet exist or is not already hardlinked or when the -f option is used.
 Missing directories in to_dir are created if needed.
 %
 	exit
 fi
+
+# Determine if we have gstat, otherwise use stat
+stat=$((which gstat 2>/dev/null)||(which stat))
 
 from_dir=$1
 to_dir=$2
@@ -36,7 +38,7 @@ esac
 
 while read file ; do
 	new=${file/#$from_dir/$to_dir}
-	if (! test $file -ot $new) || (test $force -eq 1) ; then
+	if (test $force -eq 1) || (! test -e $new) || (test $($stat --printf '%h\n' $new) -eq 1) ; then
 		echo "$file -> $new"
 		mkdir -p $(dirname $new)
 		ln -f $file $new
