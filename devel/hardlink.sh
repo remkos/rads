8e2d159a2e12cdd7fb4589b103abca713b2af2ec
Remko Scharroo <remko@altimetrics.com>
2023-10-14 23:31:50 +0200

    hardlink.sh: add -n option

--- devel/hardlink.sh
+++ devel/hardlink.sh
@@ -6,19 +6,22 @@ if test $# -le 1 ; then
 	cat <<%
 hardlink.sh - Create hardlinks of new files from one directory structure to another
 
-syntax: hardlink.sh from_dir to_dir [ -f ] [ find_options ]
+syntax: hardlink.sh from_dir to_dir [ -f | -n ] [ find_options ]
 
 where:
 	from_dir     : directory where to find new files
 	to_dir       : directory where to hardlink new files to (to_dir replaces from_dir in the pathname)
-	-f           : make link irrespective of whether the target file is already hardlinked
+	-f           : force making the link (irrespective of whether target is hardlinked)
+	-n           : make the link when the source is newer than the target (irrespective of being hardlinked)
 	find_options : options to add to the find command for example:
 	              -mtime -1 : only look for files less than 24 hour old
 	              -links 1  : only look for files that are not yet hardlinked
 
 This command first looks for files in from_dir using the find command with find_options.
-Each selected file from from_dir is harlinked to the same location in to_dir whenever
-the target does not yet exist or is not already hardlinked or when the -f option is used.
+Each selected file from from_dir is harlinked to the same location in to_dir whenever:
+* the -f flag is used;
+* the -n flag is used and the source is newer than the target;
+* the target does not yet exist or is not already hardlinked
 Missing directories in to_dir are created if needed.
 %
 	exit
@@ -30,15 +33,19 @@ stat=$((which gstat 2>/dev/null)||(which stat))
 from_dir=$1
 to_dir=$2
 force=0
+newer=0
 shift 2
 
 case "$1" in
 	"-f") force=1 ; shift 1 ;;
+	"-n") newer=1 ; shift 1 ;;
 esac
 
 while read file ; do
 	new=${file/#$from_dir/$to_dir}
-	if (test $force -eq 1) || (! test -e $new) || (test $($stat --printf '%h\n' $new) -eq 1) ; then
+	if (test $force -eq 1) || \
+		(test $newer -eq 1 && test $file -nt $new) || \
+		(! test -e $new) || (test $($stat --printf '%h\n' $new) -eq 1) ; then
 		echo "$file -> $new"
 		mkdir -p $(dirname $new)
 		ln -f $file $new
