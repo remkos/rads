a470e996e7cd064eba83e1fe1bc71b81cbe66f0d
Remko Scharroo <remko@altimetrics.com>
2024-01-26 14:42:33 +0100

    rads_add_iono: introduced --scale, removed --iri2007, apply --gim only from 1994-01-01

--- devel/rads_add_iono.f90
+++ devel/rads_add_iono.f90
@@ -38,15 +38,19 @@ type(giminfo) :: info
 ! Command line arguments
 
 character(rads_cmdl) :: path
-integer(fourbyteint) :: j, cyc, pass
+integer(fourbyteint) :: j, cyc, pass, ios
 real(eightbytereal) :: f, f_scaled
-integer(fourbyteint), parameter :: nmod = 3
+integer(fourbyteint), parameter :: nmod = 2
 logical :: model(nmod) = .false.
+
+! Other parameters
+
+real(eightbytereal), parameter :: sec1994 = 283996800d0
 
 ! Initialise
 
 call synopsis ('--head')
-call rads_set_options ('gin gim iri2007 nic09 all')
+call rads_set_options ('gn gim nic09 all scale:')
 call rads_init (S)
 
 ! Determine conversion factor from TEC units to ionospheric delay in metres
@@ -55,7 +59,6 @@ call rads_init (S)
 f = -0.4028d0/S%frequency(1)**2	! freq in GHz from rads.xml; f = factor from TEC to meters
 var => rads_varptr (S, 'iono_gim')
 read (var%info%parameters, *) f_scaled ! Get the ionosphere scaling parameter
-f_scaled = f_scaled * f
 
 ! Get ${ALTIM}/data directory
 
@@ -67,18 +70,19 @@ do j = 1,rads_nopt
 	select case (rads_opt(j)%opt)
 	case ('g', 'gim')
 		model(1) = .true.
-	case ('i', 'iri2007')
-		call rads_message ('IRI2007 no longer supported')
 	case ('n', 'nic09')
-		model(3) = .true.
+		model(2) = .true.
 	case ('all')
 		model(1) = .true.
-		model(3) = .true.
+		model(2) = .true.
+	case ('scale')
+		read (rads_opt(j)%arg, *, iostat=ios) f_scaled ! Overrule config file
 	end select
 enddo
+f_scaled = f_scaled * f
 
 if (model(1)) info = giminit(trim(path)//'gim/jplg_',1)
-if (model(3)) call nicinit(trim(path)//'nic09/nic09_clim.nc',trim(path)//'nic09/nic09_gtec.nc')
+if (model(2)) call nicinit(trim(path)//'nic09/nic09_clim.nc',trim(path)//'nic09/nic09_gtec.nc')
 
 ! Process all data files
 
@@ -103,10 +107,10 @@ call synopsis_devel (' [processing_options]')
 write (*,1310)
 1310  format (/ &
 'Additional [processing_options] are:'/ &
-'  -g, --gim                 Add JPL GIM model data' / &
+'  -g, --gim                 Add JPL GIM model data (only for 1994-01-01 and later)' / &
 '  -n, --nic09               Add NIC09 ionosphere model' / &
 '  --all                     All of the above' / &
-'  -i  --iri2007             IRI2007 ionosphere model NO LONGER SUPPORTED')
+'  --scale=SCALE             Set scale factor (default is from rads.xml)')
 stop
 end subroutine synopsis
 
@@ -116,8 +120,8 @@ end subroutine synopsis
 
 subroutine process_pass (n)
 integer(fourbyteint), intent(in) :: n
-integer(fourbyteint) :: i, j, ii, iold
-real(eightbytereal) :: time(n), lat(n), lon(n), alt(n), tec1(n), z(n,nmod), dtime, d
+integer(fourbyteint) :: i, j
+real(eightbytereal) :: time(n), lat(n), lon(n), z(n,nmod)
 logical :: ok(nmod)
 
 call log_pass (P)
@@ -127,35 +131,20 @@ call log_pass (P)
 call rads_get_var (S, P, 'time', time, .true.)
 call rads_get_var (S, P, 'lat', lat, .true.)
 call rads_get_var (S, P, 'lon', lon, .true.)
-call rads_get_var (S, P, 'alt', alt, .true.)
 
 ! Now do all models
 
-if (model(1)) then ! JPL GIM
+z = nan
+
+if (model(1) .and. time(1) > sec1994) then ! JPL GIM
 	do i = 1,n
 		z(i,1) = f_scaled * gimtec(time(i),lat(i),lon(i),info)
 	enddo
 endif
 
-if (model(2)) then ! IRI2007 (no longer supported)
-	! Compute IRI every 10 seconds, then interpolate linearly in time
-	iold = 1
+if (model(2)) then ! NIC09
 	do i = 1,n
-		dtime = time(i) - time(iold)
-		if (dtime < 10d0 .and. i > 1 .and. i < n) cycle
-!		call iri2007tec(0,time(i),lat(i),lon(i),alt(i),alt(i),tec1(i),tec2)
-		do ii = iold+1,i-1
-			d = (time(ii)-time(iold)) / dtime
-			tec1(ii) = (1d0-d) * tec1(iold) + d * tec1(i)
-		enddo
-		iold = i
-	enddo
-	z(:,2) = f * tec1
-endif
-
-if (model(3)) then ! NIC09
-	do i = 1,n
-		z(i,3) = f_scaled * nictec(time(i),lat(i),lon(i))
+		z(i,2) = f_scaled * nictec(time(i),lat(i),lon(i))
 	enddo
 endif
 
@@ -172,12 +161,10 @@ endif
 call rads_put_history (S, P)
 
 if (ok(1)) call rads_def_var (S, P, 'iono_gim')
-if (ok(2)) call rads_def_var (S, P, 'iono_iri2007')
-if (ok(3)) call rads_def_var (S, P, 'iono_nic09')
+if (ok(2)) call rads_def_var (S, P, 'iono_nic09')
 
 if (ok(1)) call rads_put_var (S, P, 'iono_gim', z(:,1))
-if (ok(2)) call rads_put_var (S, P, 'iono_iri2007', z(:,2))
-if (ok(3)) call rads_put_var (S, P, 'iono_nic09', z(:,3))
+if (ok(2)) call rads_put_var (S, P, 'iono_nic09', z(:,2))
 
 call log_records (n)
 end subroutine process_pass
