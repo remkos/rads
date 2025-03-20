a50cc75c1e2107eea3db7c2acce2f51543ba7513
Remko Scharroo <remko@altimetrics.com>
2025-03-20 13:37:41 +0100

    rads_gen_s3: updates for IPF version >= 07.11 and removed mss_comb15

--- devel/rads_gen_s3.f90
+++ devel/rads_gen_s3.f90
@@ -276,10 +276,11 @@ do
 		iono_alt_smooth = .true.
 	endif
 
-! Update the version of MSS CNES/CLS and switch on internal tide IPF-SM-2 06.51 and later
+! Update the version of MSS CNES/CLS and switch on internal tide IPF-SM-2 06.51 and later.
+! Since the Combined MSS 2015 is now obsolete, it is here simply switched off.
 
 	if (arg(10:14) >= '06.51') then
-		mss_sol1_var = 'mss_comb15'
+		mss_sol1_var = ''
 		ipf651 = .true.
 	endif
 
@@ -297,6 +298,10 @@ do
 ! Include wave model fields
 
 	ipf704 = (arg(10:14) >= '07.04')
+
+! Update MSS CNES/CLS again for IPF-SM-2 07.11
+
+	if (arg(10:14) >= '07.11') mss_sol1_var='mss_hydrid23'
 
 ! Store input file name
 
@@ -411,8 +416,10 @@ do
 	call get_var (ncid, 'geoid_01', a)
 	call new_var ('geoid_egm2008', a + dh)
 	call get_var (ncid, 'mean_sea_surf_sol1_01', a)
-	call new_var (mss_sol1_var, a + dh)
-	call get_var (ncid, 'mean_sea_surf_sol2_01', a)
+	if (mss_sol1_var /= '') then
+		call new_var (mss_sol1_var, a + dh)
+		call get_var (ncid, 'mean_sea_surf_sol2_01', a)
+	endif
 	call new_var (mss_sol2_var, a + dh)
 
 	call cpy_var (ncid, 'swh_ocean_01_ku', 'swh_ku')
