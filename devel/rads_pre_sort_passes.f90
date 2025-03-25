e1788dee5e7aa029054e61b92ffb23fd4a1ff04a
Remko Scharroo <remko.scharroo@eumetsat.int>
2025-03-25 15:04:59 +0100

    rads_pre_sort_passes: extended to 150 granules per pass

--- devel/rads_pre_sort_passes.f90
+++ devel/rads_pre_sort_passes.f90
@@ -47,7 +47,7 @@ type :: fileinfo
 	real(eightbytereal) :: time0, time1, lat0, lat1, lon0, lon1
 	character(len=rads_cmdl) :: filenm
 end type
-type(fileinfo) :: fin(75)
+type(fileinfo) :: fin(150)
 
 ! Group names
 
@@ -276,7 +276,7 @@ contains
 subroutine fill_fin (i0, i1)
 integer, intent(in) :: i0, i1
 nfile = nfile + 1
-if (nfile > 75) call rads_exit ('Number of granules too large (> 75)')
+if (nfile > 150) call rads_exit ('Number of granules too large (> 150)')
 fin(nfile)%ncid = ncid1
 fin(nfile)%nrec = nrec
 fin(nfile)%filenm = filenm
