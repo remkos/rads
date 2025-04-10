28595241155e5436cfc6b3fa074bb489b39ec588
Eric Leuliette <eric.leuliette@noaa.gov>
2025-04-10 11:38:26 -0400

    Add alternative grep for GDR-G files for rads_pre_sort_passes files

--- devel/rads_gen_6a_calval.sh
+++ devel/rads_gen_6a_calval.sh
@@ -115,6 +115,7 @@ rads_add_orbit    $options -Valt_gps --dir=jplgpsmoe -C113-299	>> "$log" 2>&1
 rads_add_orbit    $options -Valt_std2400 -C4-152                >> "$log" 2>&1
 # To support GDR-G with backward compatibility
 grep -q _G...SEN6 $lst && rads_add_tide $options --models=fes14		>> "$log" 2>&1
+grep -q .*S6A_.*_G $lst && rads_add_tide $options --models=fes14		>> "$log" 2>&1
 # Redetermine SSHA
 rads_add_refframe $options -x -x nr $extra						>> "$log" 2>&1
 rads_add_sla      $options -x -x nr $extra						>> "$log" 2>&1
