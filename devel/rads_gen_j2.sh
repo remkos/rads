137d125c211040eb3bfa937ff78a67d9c1625c14
Remko Scharroo <remko.scharroo@eumetsat.int>
2025-04-23 16:52:12 +0200

    Jason-2 GDR-F: preliminary edits
    Please note that rads_fix_jason still corrects Ku MLE4 sigma0 for attitude. Should we keep that?

--- devel/rads_gen_j2.sh
+++ devel/rads_gen_j2.sh
@@ -14,26 +14,24 @@
 # GNU Lesser General Public License for more details.
 #-----------------------------------------------------------------------
 #
-# Convert Jason-2 O/I/GDR files to RADS
+# Convert Jason-2 GDR-F files to RADS
 #
 # syntax: rads_gen_j2.sh <directories>
 #-----------------------------------------------------------------------
 . rads_sandbox.sh
 
 rads_open_sandbox j2
-lst=$SANDBOX/rads_gen_j2.lst
 
-date												>  "$log" 2>&1
+date													>  "$log" 2>&1
 
 for tar in "$@"; do
 	case "$tar" in
-		*cycle[0-9][0-9][0-9]) dir=${tar/cycle/cycle_}; mv "$tar" "$dir" ;;
 		*.txz) tar -xJf "$tar"; dir=`basename "$tar" .txz` ;;
 		*.tgz) tar -xzf "$tar"; dir=`basename "$tar" .tgz` ;;
 		*) dir="$tar" ;;
 	esac
 	ls "$dir"/JA2_???_2P*.nc > "$lst"
-	rads_gen_jason	$options < "$lst"					>> "$log" 2>&1
+	rads_gen_jason_gdrf	$options < "$lst"				>> "$log" 2>&1
 	case "$tar" in
 		*.t?z) chmod -R u+w "$dir"; rm -rf "$dir" ;;
 	esac
@@ -42,21 +40,15 @@ done
 # Do the patches to all data
 
 rads_fix_jason    $options --all						>> "$log" 2>&1
-rads_add_orbit    $options -Valt_gdre --dir=gdr-e-poe	>> "$log" 2>&1
-rads_add_orbit    $options -Valt_gdrf --dir=gdr-f-poe	>> "$log" 2>&1
-rads_add_orbit    $options -Valt_gps     -C1-327		>> "$log" 2>&1
-rads_add_orbit    $options -Valt_slcci   -C0-248		>> "$log" 2>&1
-rads_add_orbit    $options -Valt_std2400 -C1-303		>> "$log" 2>&1
-rads_add_ssb      $options --ssb=ssb_tran2012			>> "$log" 2>&1
-rads_add_iono     $options --all						>> "$log" 2>&1
 rads_add_common   $options								>> "$log" 2>&1
+rads_add_iono     $options --all						>> "$log" 2>&1
+rads_add_orbit    $options -Valt_gps     -C1-327		>> "$log" 2>&1
+rads_add_orbit    $options -Valt_std2400 -C1-303		>> "$log" 2>&1
 rads_add_tide     $options --models=fes14				>> "$log" 2>&1
 rads_add_mog2d    $options --ymd=19910101,20160101 -ue  >> "$log" 2>&1
-rads_add_dual     $options -l							>> "$log" 2>&1
-rads_add_dual     $options -l --ext=mle3				>> "$log" 2>&1
 # Redetermine SSHA
-rads_add_refframe $options -x -x mle3					>> "$log" 2>&1
-rads_add_sla      $options -x -x mle3					>> "$log" 2>&1
+rads_add_refframe $options -x -x mle3 -x adaptive		>> "$log" 2>&1
+rads_add_sla      $options -x -x mle3 -x adaptive		>> "$log" 2>&1
 
 date													>> "$log" 2>&1
 
